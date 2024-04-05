c46116e5a6c43afc7677cc87c64ec0bd45f310da
dawawinn7 <143560740+dawawinn7@users.noreply.github.com>
2024-04-05 14:09:11 +0000

    new

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ document.getElementById('funButton').addEventListener('click', function() {
     const funText = document.getElementById('funText');
     funText.innerHTML = '🎉 Congratulations on taking your first step into web development with CS 140! 🎉';
 });
+function areaOfSquareSizeNegativeTwenty(){
+let side = 20 ;
+let area = side * side ;
+// this does the same thing , area is area^2
+// math.pow (side 2)
+alert(" the area of square with a side of 20 is : " + area);
+if(side <= 0 ){
+alert("Area must be larger than 0.")
+}
+else{
+    alert(" The area of a square with a side of 20 is: " + area) ;
+}
+}
